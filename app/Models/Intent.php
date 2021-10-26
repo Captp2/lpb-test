5aa08f7b95a8fcb3cc315a94fd26a044aedb00ac
Axel Gaudot <axel.gaudot@epitech.eu>
2021-10-26 19:58:30 +0200

    Models generated and relationships done

---
+++ app/Models/Intent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Intent extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'title',
+        'status',
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function trainingPhrases(): HasMany
+    {
+        return $this->hasMany(TrainingPhrase::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function answers(): BelongsToMany
+    {
+        return $this->belongsToMany(Answer::class);
+    }
+}
