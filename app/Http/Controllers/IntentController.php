5e2c5d5ad56ac74a48eebcde05255098bbe8d65e
Axel Gaudot <axel.gaudot@epitech.eu>
2021-10-26 21:34:53 +0200

    Front done as well as some refactoring

--- app/Http/Controllers/IntentController.php
+++ app/Http/Controllers/IntentController.php
@@ -3,37 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Models\Intent;
-use http\Exception\InvalidArgumentException;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use InvalidArgumentException;
 
 class IntentController extends Controller
 {
+    const SUPPORTED_ORDERS = [
+        self::ORDER_ANSWERS_COUNT,
+        self::ORDER_STATUS,
+        self::ORDER_TITLE,
+        self::ORDER_CREATED_AT,
+        self::ORDER_UPDATED_AT,
+    ];
+
+    const ORDER_ANSWERS_COUNT = 'answers_count';
+    const ORDER_STATUS = 'status';
+    const ORDER_TITLE = 'title';
+    const ORDER_CREATED_AT = 'created_at';
+    const ORDER_UPDATED_AT = 'updated_at';
+
     /**
-     * @return \Illuminate\Http\JsonResponse
+     * @param Request $request
+     * @return JsonResponse
      */
     public function index(Request $request): JsonResponse
     {
-        $queryBuilder = Intent::query();
-        $queryBuilder->withCount('answers');
         try {
-            $this->buildOrder($request, $queryBuilder);
+            $intentCollection = $this->buildQuery($request);
         } catch (\InvalidArgumentException $e) {
             return response()->json('Invalid order by supplied', 400);
         }
 
-        return response()->json($queryBuilder->limit(4)->get());
+        return response()->json($intentCollection);
     }
 
     /**
      * @param Request $request
-     * @param Builder $queryBuilder
+     * @return Collection
      */
-    private function buildOrder(Request $request, Builder $queryBuilder): void
+    private function buildQuery(Request $request): Collection
     {
+        $queryBuilder = Intent::query();
+        $queryBuilder->withCount('answers');
+
         foreach ($request->get('orderBy', ['title']) as $order) {
-            if (is_string($order)) {
+            if (in_array($order, self::SUPPORTED_ORDERS)) {
                 // If we ever need more special orders
                 switch ($order) {
                     case 'answers_count':
@@ -46,5 +63,22 @@ class IntentController extends Controller
                 throw new InvalidArgumentException();
             }
         }
+
+        return $queryBuilder->limit(4)->get();
+    }
+
+    /**
+     * @param Request $request
+     * @return View
+     */
+    public function view(Request $request): View
+    {
+        try {
+            $intentCollection = $this->buildQuery($request);
+        } catch (\InvalidArgumentException $e) {
+            return view('intents', ['error' => 'Invalid order by supplied']);
+        }
+
+        return view('intents', ['intents' => $intentCollection]);
     }
 }
